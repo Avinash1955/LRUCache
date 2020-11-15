1600eeba5782e3e079201dc773f1ca978b00213c
Avinash <avinash.kumar1403@gmail.com>
2020-11-15 18:34:29 +0000

    LRU Cache Implementation
    LRU Cache Implementation

---
+++ CachingUnitTest/CachingUnitTest.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LRUCache;
+
+namespace CachingUnitTest
+{
+    [TestClass]
+    public class LRUTest
+    {
+        private const int capacity = 5;
+        private LruCache<int,string> lruCache = new LruCache<int,string>(capacity);
+
+        [TestMethod]
+        public void AddandRetrieveValue()
+        {
+            lruCache.Add(0, "Test0");
+            string value;
+            bool result = lruCache.TryGetValue(0, out value);
+            Assert.IsTrue(result);
+            Assert.AreEqual(value, "Test0");
+        }
+
+        [TestMethod]
+        public void UpdateValue()
+        {
+            lruCache.Add(0, "Test0");
+            lruCache.Add(1, "Test1");
+            lruCache.Add(1, "Test11");
+            string value;
+            bool result = lruCache.TryGetValue(1, out value);
+            Assert.IsTrue(result);
+            Assert.AreEqual(value, "Test11");
+
+        }
+
+        [TestMethod]
+        public void GetValueKeyNotPresent()
+        {
+            lruCache.Add(0, "Test0");
+            lruCache.Add(1, "Test1");
+            string value;
+            bool result = lruCache.TryGetValue(2, out value);
+            Assert.IsFalse(result);
+            Assert.AreEqual(value,null);
+
+        }
+
+        [TestMethod]
+        public void MaxCapacityAndRemoveLRUTest()
+        {
+            for(int i=0;i<capacity;i++)
+            {
+                lruCache.Add(i,"Item"+i);
+            }
+            lruCache.Add(capacity, "Test"+ capacity);
+            string value;
+            bool result = lruCache.TryGetValue(0, out value);
+            Assert.IsFalse(result);
+            result = lruCache.TryGetValue(capacity, out value);
+            Assert.IsTrue(result);
+            Assert.AreEqual(value, "Test" + capacity);
+
+        }
+
+        [TestMethod]
+        public void RemoveElementTest()
+        {
+            string value;
+            lruCache.Add(20, "Test20");
+            bool result = lruCache.TryGetValue(20, out value);
+            Assert.IsTrue(result);
+            lruCache.Remove(20);
+            result = lruCache.TryGetValue(20, out value);
+            Assert.IsFalse(result);
+
+        }
+
+
+    }
+}
---
+++ LRUCache/LruCache.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LRUCache
+{
+    public class LruCache<TKey, TValue>
+    {
+        private class cacheItem
+        {
+            public cacheItem(TKey k, TValue v)
+            {
+                this.Key = k;
+                this.Value = v;
+            }
+
+            public TKey Key { get; }
+
+            public TValue Value { get; set; }
+        }
+
+        public int Capacity { get; }
+        private readonly Dictionary<TKey, LinkedListNode<cacheItem>> elements = new Dictionary<TKey, LinkedListNode<cacheItem>>();
+        private readonly LinkedList<cacheItem> lruList = new LinkedList<cacheItem>();
+        private readonly Action<TKey, TValue> itemEvictHandler;
+
+        public LruCache(int capacity, Action<TKey,TValue> evictHandler = null)
+        {
+            Capacity = capacity;
+            itemEvictHandler = evictHandler;
+        }
+
+
+        /// <summary>Gets the value associated with the specified key.</summary>
+        /// <param name="key">
+        /// The key of the value to get.
+        /// </param>
+        /// <param name="value">
+        /// When this method returns, contains the value associated with the specified
+        /// key, if the key is found; otherwise, the default value for the type of the 
+        /// </param>
+        /// <returns>
+        /// true if the cache contains an element with the specified key; otherwise, false.
+        /// </returns>
+        
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            lock(elements)
+            {
+                LinkedListNode<cacheItem> itemNode;
+                if (elements.TryGetValue(key, out itemNode))
+                {
+                    value = itemNode.Value.Value;
+                    lruList.Remove(itemNode);
+                    lruList.AddFirst(itemNode);
+                    return true;
+                }
+
+                value = default(TValue);
+                return false;
+            }
+        }
+
+
+        /// <summary>
+        /// Add the given key and value to LRU Cache.
+        /// If the element with given key is already present then value is updated.
+        /// </summary>
+        /// <param name="key">Key of element to be added.</param>
+        /// <param name="value">Value of element to be added.</param>
+        public void Add(TKey key, TValue value)
+        {
+            lock (elements)
+            {
+                LinkedListNode<cacheItem> itemNode;
+                if (elements.TryGetValue(key, out itemNode))
+                {
+                    itemNode.Value.Value = value;
+                    lruList.Remove(itemNode);
+                    lruList.AddFirst(itemNode);
+                }
+                else
+                {
+                    if (elements.Count >= this.Capacity)
+                    {
+                        Remove(lruList.Last.Value.Key);
+                       
+                    }
+
+                    cacheItem item = new cacheItem(key, value);
+                    LinkedListNode<cacheItem> node = new LinkedListNode<cacheItem>(item);
+                    lruList.AddFirst(node);
+                    elements.Add(key, node);
+                    
+                }
+            }
+
+        }
+
+
+        /// <summary>
+        /// Remove element for given Key from Cache
+        /// </summary>
+        /// <param name="key"></param>
+        public void Remove(TKey key)
+        {
+            lock (elements)
+            {
+                LinkedListNode<cacheItem> elementNode;
+                if (elements.TryGetValue(key, out elementNode))
+                {
+                    lruList.Remove(elementNode);
+                    elements.Remove(key);
+                    this.itemEvictHandler?.Invoke(key, elementNode.Value.Value);
+
+                    // If the object is disposable then invoke Dispose
+
+                    if (elementNode.Value.Value is IDisposable d)
+                    {
+                        d.Dispose();
+                    }
+                }
+            }
+
+        }
+
+
+    }
+}
